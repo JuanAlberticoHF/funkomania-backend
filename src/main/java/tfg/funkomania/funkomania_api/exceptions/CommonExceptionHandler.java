6d2bca920d276a0a17395ca6533384d22c978d6f
Juan <juanalbertoh.fernandez13@gmail.com>
2026-05-30 13:24:57 +0200

    refactor: elimina importaciones innecesarias en el manejador de excepciones

--- src/main/java/tfg/funkomania/funkomania_api/exceptions/CommonExceptionHandler.java
+++ src/main/java/tfg/funkomania/funkomania_api/exceptions/CommonExceptionHandler.java
@@ -4,7 +4,6 @@ import jakarta.validation.ConstraintViolationException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
