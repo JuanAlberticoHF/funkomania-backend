0aeef1c8f2447619a31aa69d9efddce89fb19549
Juan <juanalbertoh.fernandez13@gmail.com>
2026-05-31 13:44:10 +0200

    feat: añade un nuevo metodo al repositorio de usuarios

--- src/main/java/tfg/funkomania/funkomania_api/persistence/repositories/IUsuarioRepository.java
+++ src/main/java/tfg/funkomania/funkomania_api/persistence/repositories/IUsuarioRepository.java
@@ -3,13 +3,16 @@ package tfg.funkomania.funkomania_api.persistence.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tfg.funkomania.funkomania_api.persistence.entities.Usuario;
 
+import java.util.Optional;
+
 /**
  * Interfaz de repositorio para la entidad Usuario.
  *
  * @author JuanAlbeticoHF
- * @version 0.1.0
+ * @version 0.2.0
  * @since 0.1.0
  */
 public interface IUsuarioRepository extends JpaRepository<Usuario, Long> {
     boolean existsByEmail(String email);
+    Optional<Usuario> findUsuarioByEmail(String email);
 }
