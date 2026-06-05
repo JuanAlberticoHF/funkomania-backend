5ff1638cdab89806b33f110826efa28b20e29532
Juan <juanalbertoh.fernandez13@gmail.com>
2026-06-05 20:09:07 +0200

    feat: actualiza el método isNullOrEmpty en UsuarioUpdateRequestDTO para usar un parámetro DTO y actualiza la versión a 1.1.1

--- src/main/java/tfg/funkomania/funkomania_api/dtos/usuario_dtos/UsuarioUpdateRequestDTO.java
+++ src/main/java/tfg/funkomania/funkomania_api/dtos/usuario_dtos/UsuarioUpdateRequestDTO.java
@@ -1,5 +1,6 @@
 package tfg.funkomania.funkomania_api.dtos.usuario_dtos;
 
+import jakarta.validation.constraints.Null;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -9,7 +10,7 @@ import lombok.NoArgsConstructor;
  * DTO para la actualización de un usuario. Contiene los campos que pueden ser actualizados por el usuario autenticado.
  *
  * @author JuanAlbeticoHF
- * @version 1.1.0
+ * @version 1.1.1
  * @since 0.4.0
  */
 @NoArgsConstructor
@@ -44,10 +45,10 @@ public class UsuarioUpdateRequestDTO {
      * Verifica que todos los campos del DTO sean nulos o el nombre este vacío.
      * @return true si todos los campos son nulos o el nombre esta vacío, false en caso contrario.
      */
-    public boolean isNullOrEmpty() {
-        return (nombre == null || nombre.isEmpty()) &&
-               (apellido1 == null) &&
-               (apellido2 == null) &&
-               (telefono == null);
+    public static boolean isNullOrEmpty(UsuarioUpdateRequestDTO dto) {
+        return (dto.getNombre() == null || dto.getNombre().isEmpty()) &&
+               (dto.getApellido1() == null) &&
+               (dto.getApellido2() == null) &&
+               (dto.getTelefono() == null);
     }
 }
