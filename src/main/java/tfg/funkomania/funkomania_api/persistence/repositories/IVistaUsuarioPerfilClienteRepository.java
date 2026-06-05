36d9f9d74da82c058aa90332a4bb2e1771aa527c
Juan <juanalbertoh.fernandez13@gmail.com>
2026-06-05 18:28:29 +0200

    feat: cambia el método findByEmail para devolver un Optional en IVistaUsuarioPerfilClienteRepository

--- src/main/java/tfg/funkomania/funkomania_api/persistence/repositories/IVistaUsuarioPerfilClienteRepository.java
+++ src/main/java/tfg/funkomania/funkomania_api/persistence/repositories/IVistaUsuarioPerfilClienteRepository.java
@@ -3,6 +3,15 @@ package tfg.funkomania.funkomania_api.persistence.repositories;
 import org.springframework.data.repository.Repository;
 import tfg.funkomania.funkomania_api.persistence.entities.VistaUsuarioPerfilCliente;
 
+import java.util.Optional;
+
+/**
+ * Interfaz de repositorio para la entidad VistaUsuarioPerfilCliente.
+ *
+ * @author JuanAlbeticoHF
+ * @version 1.0.2
+ * @since 0.4.0
+ */
 public interface IVistaUsuarioPerfilClienteRepository extends Repository<VistaUsuarioPerfilCliente, Long> {
-    VistaUsuarioPerfilCliente findByEmail(String email);
+    Optional<VistaUsuarioPerfilCliente> findByEmail(String email);
 }
