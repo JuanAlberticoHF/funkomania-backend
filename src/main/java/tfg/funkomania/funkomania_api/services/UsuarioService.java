fd7d75ef1447ca4680ee9ac279a490d8e12fe5f4
Juan <juanalbertoh.fernandez13@gmail.com>
2026-06-05 18:16:44 +0200

    feat: actualiza el método obtenerPerfilClienteAutenticado para usar VistaUsuarioPerfilClienteDTOId

--- src/main/java/tfg/funkomania/funkomania_api/services/UsuarioService.java
+++ src/main/java/tfg/funkomania/funkomania_api/services/UsuarioService.java
@@ -1,13 +1,13 @@
 package tfg.funkomania.funkomania_api.services;
 
 import tfg.funkomania.funkomania_api.dtos.usuario_dtos.UsuarioUpdateRequestDTO;
-import tfg.funkomania.funkomania_api.persistence.entities.VistaUsuarioPerfilCliente;
+import tfg.funkomania.funkomania_api.dtos.usuario_dtos.VistaUsuarioPerfilClienteDTOId;
 
 /**
  * Interfaz de servicio para la gestión de usuarios en la aplicación Funkomania.
  *
  * @author JuanAlbeticoHF
- * @version 1.0.0
+ * @version 1.0.1
  * @since 0.4.0
  */
 public interface UsuarioService {
@@ -15,7 +15,7 @@ public interface UsuarioService {
      * Obtiene los datos del perfil del cliente autenticado.
      * @return El perfil del cliente autenticado, o null si no hay un cliente autenticado.
      */
-    VistaUsuarioPerfilCliente obtenerPerfilClienteAutenticado();
+    VistaUsuarioPerfilClienteDTOId obtenerPerfilClienteAutenticado();
 
     /**
      * Actualiza los datos del usuario autenticado con la información proporcionada en el DTO de actualización.
