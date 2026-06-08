05c598b872da723bde82b3c96d0f62e9727cc26f
Juan <juanalbertoh.fernandez13@gmail.com>
2026-06-08 21:55:33 +0200

    feat: añade DTO para crear y actualizar categorías, y métodos en el servicio para gestionar categorías

--- src/main/java/tfg/funkomania/funkomania_api/services/CategoriaService.java
+++ src/main/java/tfg/funkomania/funkomania_api/services/CategoriaService.java
@@ -1,5 +1,7 @@
 package tfg.funkomania.funkomania_api.services;
 
+import tfg.funkomania.funkomania_api.dtos.categoria_dtos.CategoriaDTORequest;
+import tfg.funkomania.funkomania_api.dtos.producto_dtos.VistaProductosCatalogoDTOId;
 import tfg.funkomania.funkomania_api.persistence.entities.Categoria;
 
 import java.util.List;
@@ -9,7 +11,7 @@ import java.util.List;
  * Define los métodos para realizar operaciones relacionadas con las categorías de productos.
  *
  * @author JuanAlbeticoHF
- * @version 1.0.0
+ * @version 1.1.0
  * @since 0.2.0
  */
 public interface CategoriaService {
@@ -18,4 +20,30 @@ public interface CategoriaService {
      * @return Una lista de objetos Categoria que representan todas las categorías disponibles en el sistema.
      */
     List<Categoria> getAllCategorias();
+
+    /**
+     * Obtiene una categoría por su ID, incluyendo los productos asociados a esa categoría.
+     * @param idCategoria El ID de la categoría que se desea obtener.
+     * @return Una lista de objetos VistaProductosCatalogoDTOId que representan los productos asociados a la categoría especificada. Si la categoría no existe, se devuelve una lista vacía.
+     */
+    List<VistaProductosCatalogoDTOId> obtenerProductosAsociadosDeUnaCategoria(Long idCategoria);
+
+    /**
+     * Crea una nueva categoría en el sistema a partir de un objeto CategoriaDTORequest.
+     * @param categoriaDTORequest El objeto CategoriaDTORequest que contiene la información de la categoría a crear.
+     */
+    void crearCategoria(CategoriaDTORequest categoriaDTORequest);
+
+    /**
+     * Elimina una categoría del sistema por su ID.
+     * @param idCategoria El ID de la categoría que se desea eliminar.
+     * @param categoriaDTORequest El objeto CategoriaDTORequest que contiene el ID de la categoría a eliminar.
+     */
+    void actualizarCategoria(Long idCategoria, CategoriaDTORequest categoriaDTORequest);
+
+    /**
+     * Elimina una categoría del sistema por su ID.
+     * @param idCategoria El ID de la categoría que se desea eliminar.
+     */
+    void eliminarCategoria(Long idCategoria);
 }
--- src/main/java/tfg/funkomania/funkomania_api/services/CategoriaServiceImpl.java
+++ src/main/java/tfg/funkomania/funkomania_api/services/CategoriaServiceImpl.java
@@ -1,6 +1,11 @@
 package tfg.funkomania.funkomania_api.services;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import tfg.funkomania.funkomania_api.dtos.categoria_dtos.CategoriaDTORequest;
+import tfg.funkomania.funkomania_api.dtos.producto_dtos.VistaProductosCatalogoDTOId;
+import tfg.funkomania.funkomania_api.exceptions.custom_exceptions.CategoriaConProductosException;
+import tfg.funkomania.funkomania_api.exceptions.custom_exceptions.CategoriaNotFoundException;
 import tfg.funkomania.funkomania_api.persistence.entities.Categoria;
 import tfg.funkomania.funkomania_api.persistence.repositories.ICategoriaRepository;
 
@@ -28,5 +33,69 @@ public class CategoriaServiceImpl implements CategoriaService {
     @Override
     public List<Categoria> getAllCategorias() {
         return categoriaRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<VistaProductosCatalogoDTOId> obtenerProductosAsociadosDeUnaCategoria(Long idCategoria) {
+        return categoriaRepository.findCategoriaByIdConProductosParaAdmin(idCategoria)
+                .map(categoria -> categoria.getProductosAsociados().stream()
+                        .map(VistaProductosCatalogoDTOId::new)
+                        .toList())
+                .orElse(List.of());
+    }
+
+    @Transactional
+    @Override
+    public void crearCategoria(CategoriaDTORequest categoriaDTORequest) {
+        // Verificar si la categoría padre existe antes de crear la nueva categoría
+        Categoria categoriaPadre = null;
+
+        if (categoriaDTORequest.idCategoriaPadre() != null) {
+            categoriaPadre = categoriaRepository.findById(categoriaDTORequest.idCategoriaPadre())
+                    .orElseThrow(() -> new CategoriaNotFoundException("La categoría padre con ID " + categoriaDTORequest.idCategoriaPadre() + " no existe."));
+        }
+
+        // Crear la nueva categoría con la referencia a la categoría padre (si existe)
+        categoriaRepository.save(new Categoria(null, categoriaDTORequest.nombre(), categoriaPadre, null));
+    }
+
+    @Transactional
+    @Override
+    public void actualizarCategoria(Long idCategoria, CategoriaDTORequest categoriaDTORequest) {
+        // Verificamos que la categoria existe
+        Categoria categoria = categoriaRepository.findById(idCategoria)
+                .orElseThrow(() -> new CategoriaNotFoundException("La categoría con ID " + idCategoria + " no existe."));
+
+        // Verificar si la categoría padre existe antes de actualizar la categoría
+        Categoria categoriaPadre = null;
+
+        if (categoriaDTORequest.idCategoriaPadre() != null) {
+            categoriaPadre = categoriaRepository.findById(categoriaDTORequest.idCategoriaPadre())
+                    .orElseThrow(() -> new CategoriaNotFoundException("La categoría padre con ID " + categoriaDTORequest.idCategoriaPadre() + " no existe."));
+        }
+
+        // Actualizar los campos de la categoría
+        categoria.setNombre(categoriaDTORequest.nombre());
+        categoria.setCategoriaPadre(categoriaPadre);
+
+        // Crear la nueva categoría con la referencia a la categoría padre (si existe)
+        categoriaRepository.save(categoria);
+    }
+
+    @Transactional
+    @Override
+    public void eliminarCategoria(Long idCategoria) {
+        // Verificamos que la categoria existe
+        Categoria categoria = categoriaRepository.findById(idCategoria)
+                .orElseThrow(() -> new CategoriaNotFoundException("La categoría con ID " + idCategoria + " no existe."));
+
+        // Verificar si la categoría tiene productos asociados antes de eliminarla
+        if (categoriaRepository.tieneProductosAsociados(idCategoria)) {
+            throw new CategoriaConProductosException("No se puede eliminar la categoría con ID " + idCategoria + " porque tiene productos asociados.");
+        }
+
+        // Eliminar la categoría
+        categoriaRepository.delete(categoria);
     }
 }
