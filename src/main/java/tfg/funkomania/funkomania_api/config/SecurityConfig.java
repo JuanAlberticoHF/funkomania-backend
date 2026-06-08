6a879b4d1d63cf1ec0ccb7f3be8799bb6f75291b
Juan <juanalbertoh.fernandez13@gmail.com>
2026-06-08 17:13:38 +0200

    feat: añade controlador para gestionar métodos de pago activos

--- src/main/java/tfg/funkomania/funkomania_api/config/SecurityConfig.java
+++ src/main/java/tfg/funkomania/funkomania_api/config/SecurityConfig.java
@@ -69,6 +69,7 @@ public class SecurityConfig {
                         .requestMatchers("/usuario/direcciones/**").authenticated()
                         .requestMatchers("/usuario/notificaciones/**").authenticated()
                         .requestMatchers("/usuario/lista-deseos/**").authenticated()
+                        .requestMatchers("/metodos-pago/**").permitAll()
                         // El acceso a los endpoints para cualquier otra solicitud necesita autenticación.
                         .anyRequest().authenticated()
                 )
