3592593e7775256029cb55bb6771138ff53588a5
Juan <juanalbertoh.fernandez13@gmail.com>
2026-06-08 14:13:34 +0200

    feat: añade método para obtener notificaciones por ID de usuario en IVistaNotificacionesUsuarioRepository

--- src/main/java/tfg/funkomania/funkomania_api/persistence/repositories/IVistaNotificacionesUsuarioRepository.java
+++ src/main/java/tfg/funkomania/funkomania_api/persistence/repositories/IVistaNotificacionesUsuarioRepository.java
@@ -3,6 +3,8 @@ package tfg.funkomania.funkomania_api.persistence.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tfg.funkomania.funkomania_api.persistence.entities.VistaNotificacionesUsuarios;
 
+import java.util.List;
+
 /**
  * Interfaz de repositorio para la entidad VistaNotificacionesUsuarios.
  *
@@ -11,4 +13,5 @@ import tfg.funkomania.funkomania_api.persistence.entities.VistaNotificacionesUsu
  * @since 0.5.0
  */
 public interface IVistaNotificacionesUsuarioRepository extends JpaRepository<VistaNotificacionesUsuarios, Long> {
+    List<VistaNotificacionesUsuarios> findVistaNotificacionesUsuariosByIdUsuario(Long idUsuario);
 }
