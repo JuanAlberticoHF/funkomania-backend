58ea33926d50c1a15aeb6e20a4086a83900c0caf
Juan <juanalbertoh.fernandez13@gmail.com>
2026-06-09 13:12:49 +0200

    feat: añade el método obtenerTodosLosUsuarios al servicio y su implementación

--- src/main/java/tfg/funkomania/funkomania_api/services/UsuarioService.java
+++ src/main/java/tfg/funkomania/funkomania_api/services/UsuarioService.java
@@ -1,13 +1,16 @@
 package tfg.funkomania.funkomania_api.services;
 
+import tfg.funkomania.funkomania_api.dtos.usuario_dtos.UsuarioDTOId;
 import tfg.funkomania.funkomania_api.dtos.usuario_dtos.UsuarioUpdateRequestDTO;
 import tfg.funkomania.funkomania_api.dtos.usuario_dtos.VistaUsuarioPerfilClienteDTOId;
+
+import java.util.List;
 
 /**
  * Interfaz de servicio para la gestión de usuarios en la aplicación Funkomania.
  *
  * @author JuanAlbeticoHF
- * @version 1.0.1
+ * @version 1.1.0
  * @since 0.4.0
  */
 public interface UsuarioService {
@@ -22,4 +25,12 @@ public interface UsuarioService {
      * @param usuarioUpdateRequestDTO El DTO que contiene la información actualizada del usuario. No debe ser nulo y debe contener datos válidos.
      */
     void actualizarUsuarioAutenticado(UsuarioUpdateRequestDTO usuarioUpdateRequestDTO);
+
+    /**
+     * Obtiene una lista de todos los usuarios registrados en el sistema, con la opción de filtrar por un término de búsqueda.
+     * @param search Un término de búsqueda opcional para filtrar los usuarios por nombre,
+     * @return Una lista de objetos UsuarioDTOId que representan a los usuarios registrados en el sistema,
+     * filtrados por el término de búsqueda si se proporciona.
+     */
+    List<UsuarioDTOId> obtenerTodosLosUsuarios(String search);
 }
--- src/main/java/tfg/funkomania/funkomania_api/services/UsuarioServiceImpl.java
+++ src/main/java/tfg/funkomania/funkomania_api/services/UsuarioServiceImpl.java
@@ -1,7 +1,9 @@
 package tfg.funkomania.funkomania_api.services;
 
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import tfg.funkomania.funkomania_api.dtos.usuario_dtos.UsuarioDTOId;
 import tfg.funkomania.funkomania_api.dtos.usuario_dtos.UsuarioUpdateRequestDTO;
 import tfg.funkomania.funkomania_api.dtos.usuario_dtos.VistaUsuarioPerfilClienteDTOId;
 import tfg.funkomania.funkomania_api.exceptions.custom_exceptions.NullEmailAutenticationException;
@@ -10,6 +12,9 @@ import tfg.funkomania.funkomania_api.persistence.entities.Usuario;
 import tfg.funkomania.funkomania_api.persistence.entities.VistaUsuarioPerfilCliente;
 import tfg.funkomania.funkomania_api.persistence.repositories.IUsuarioRepository;
 import tfg.funkomania.funkomania_api.persistence.repositories.IVistaUsuarioPerfilClienteRepository;
+import tfg.funkomania.funkomania_api.persistence.specifications.UsuarioSpecification;
+
+import java.util.List;
 
 /**
  * <p>Servicio para gestionar las operaciones de usuarios</p>
@@ -17,7 +22,7 @@ import tfg.funkomania.funkomania_api.persistence.repositories.IVistaUsuarioPerfi
  * relacionadas con los datos del usuario y la vistas de usuarios.</p>
  *
  * @author JuanAlbeticoHF
- * @version 1.1.1
+ * @version 1.2.0
  * @since 0.4.0
  */
 @Service
@@ -103,5 +108,26 @@ public class UsuarioServiceImpl implements UsuarioService {
 
         // Guardamos el usuario actualizado en la base de datos
         usuarioRepository.save(usuario);
+    }
+
+    public List<UsuarioDTOId> obtenerTodosLosUsuarios(String search) {
+        // Creamos una especificación vacía para construir la consulta dinámica
+        Specification<Usuario> spec = null;
+
+        // Agregar filtros a la especificación según los parámetros de búsqueda
+        // - Si se proporciona un término de búsqueda, filtrar por email.
+        if (search != null && !search.isEmpty()) {
+            spec = UsuarioSpecification.busquedaContiene(search);
+        }
+
+        // Creamos una lista de usuarios que recibirá la busqueda con o sin especificaciones.
+        List<Usuario> usuarioList;
+
+        // Si no se ha creado ninguna especificación, obtenemos todos los usuarios sin filtrar.
+        if (spec == null) usuarioList = usuarioRepository.findAll();
+        else usuarioList = usuarioRepository.findAll(spec);
+
+        // Devolvemos el listado de usuarios en formado UsuarioDTOId
+        return usuarioList.stream().map(UsuarioDTOId::new).toList();
     }
 }
