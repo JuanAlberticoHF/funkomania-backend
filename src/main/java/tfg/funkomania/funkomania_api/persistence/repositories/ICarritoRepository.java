73547785bb31cfd5cd74883f0205effd7c6ae124
Juan <juanalbertoh.fernandez13@gmail.com>
2026-06-12 00:25:38 +0200

    fix: añade la anotación @Modifying para poder ejecutar la sentencia de actualización.

--- src/main/java/tfg/funkomania/funkomania_api/persistence/repositories/ICarritoRepository.java
+++ src/main/java/tfg/funkomania/funkomania_api/persistence/repositories/ICarritoRepository.java
@@ -1,6 +1,7 @@
 package tfg.funkomania.funkomania_api.persistence.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import tfg.funkomania.funkomania_api.persistence.entities.Carrito;
 
@@ -12,6 +13,7 @@ import tfg.funkomania.funkomania_api.persistence.entities.Carrito;
  * @since 0.7.0
  */
 public interface ICarritoRepository extends JpaRepository<Carrito, Long> {
+    @Modifying
     @Query("UPDATE Carrito c SET c.fechaActualizacion = CURRENT_TIMESTAMP WHERE c.idCarrito = :idCarrito")
-    boolean updateFechaActualizacionByIdCarrito(Long idCarrito);
+    void updateFechaActualizacionByIdCarrito(Long idCarrito);
 }
