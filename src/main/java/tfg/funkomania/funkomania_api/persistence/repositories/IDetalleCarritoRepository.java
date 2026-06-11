3e8bdafc3ee4cf4fc80bfe2ac75a9598e3f598d5
Juan <juanalbertoh.fernandez13@gmail.com>
2026-06-12 00:25:08 +0200

    fix: modifica el retorno a void para

--- src/main/java/tfg/funkomania/funkomania_api/persistence/repositories/IDetalleCarritoRepository.java
+++ src/main/java/tfg/funkomania/funkomania_api/persistence/repositories/IDetalleCarritoRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import tfg.funkomania.funkomania_api.dtos.carrito_dtos.CarritoDTOId_IdProducto;
 import tfg.funkomania.funkomania_api.persistence.entities.Carrito;
 import tfg.funkomania.funkomania_api.persistence.entities.DetalleCarrito;
 import tfg.funkomania.funkomania_api.persistence.entities.DetalleCarritoId;
@@ -13,13 +12,13 @@ import tfg.funkomania.funkomania_api.persistence.entities.DetalleCarritoId;
  * Interfaz de repositorio para la entidad DetalleCarrito.
  *
  * @author JuanAlbeticoHF
- * @version 1.2.0
+ * @version 1.2.1
  * @since 0.7.0
  */
 public interface IDetalleCarritoRepository extends JpaRepository<DetalleCarrito, DetalleCarritoId> {
     @Modifying
     @Query(value = "CALL sp_agregar_producto_carrito(:p_idUsuario, :p_idProducto, :p_cantidad)", nativeQuery = true)
-    CarritoDTOId_IdProducto agregarProductoAlCarrito(@Param("p_idUsuario") Long idUsuario, @Param("p_idProducto") Long idProducto, @Param("p_cantidad") Integer cantidad);
+    void agregarProductoAlCarrito(@Param("p_idUsuario") Long idUsuario, @Param("p_idProducto") Long idProducto, @Param("p_cantidad") Integer cantidad);
 
     void deleteDetalleCarritosByCarrito(Carrito carrito);
 }
