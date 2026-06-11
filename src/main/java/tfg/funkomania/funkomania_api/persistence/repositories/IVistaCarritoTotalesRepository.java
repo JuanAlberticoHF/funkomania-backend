2a753512759409a25edef8faf168161e804ef262
Juan <juanalbertoh.fernandez13@gmail.com>
2026-06-12 00:24:38 +0200

    feat: añade Optional al metood findByIdCarrito de VistaCarritoTotales

--- src/main/java/tfg/funkomania/funkomania_api/persistence/repositories/IVistaCarritoTotalesRepository.java
+++ src/main/java/tfg/funkomania/funkomania_api/persistence/repositories/IVistaCarritoTotalesRepository.java
@@ -3,13 +3,15 @@ package tfg.funkomania.funkomania_api.persistence.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tfg.funkomania.funkomania_api.persistence.entities.VistaCarritoTotales;
 
+import java.util.Optional;
+
 /**
  * Interfaz de repositorio para la entidad VistaCarritoTotales.
  *
  * @author JuanAlbeticoHF
- * @version 1.0.2
+ * @version 1.0.3
  * @since 0.7.0
  */
 public interface IVistaCarritoTotalesRepository extends JpaRepository<VistaCarritoTotales, Long> {
-    VistaCarritoTotales findByIdCarrito(Long idCarrito);
+    Optional<VistaCarritoTotales> findByIdCarrito(Long idCarrito);
 }
