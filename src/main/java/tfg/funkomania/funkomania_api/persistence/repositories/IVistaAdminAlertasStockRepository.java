e52a3786149eea4d1a713aa863fa11a24dc1b97b
Juan <juanalbertoh.fernandez13@gmail.com>
2026-06-12 14:47:35 +0200

    feat: añade los repositorios de las vistas AdminAlertasStock y VistaPedidosAdmin

---
+++ src/main/java/tfg/funkomania/funkomania_api/persistence/repositories/IVistaAdminAlertasStockRepository.java
@@ -0,0 +1,14 @@
+package tfg.funkomania.funkomania_api.persistence.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import tfg.funkomania.funkomania_api.persistence.entities.VistaAdminAlertasStock;
+
+/**
+ * Interfaz de repositorio para la entidad {@link VistaAdminAlertasStock}.
+ *
+ * @author JuanAlbeticoHF
+ * @version 1.0.0
+ * @since 0.7.0
+ */
+public interface IVistaAdminAlertasStockRepository extends JpaRepository<VistaAdminAlertasStock, Long> {
+}
