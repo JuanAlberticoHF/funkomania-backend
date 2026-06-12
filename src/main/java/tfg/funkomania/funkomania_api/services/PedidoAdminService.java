e8fd4a8a86c041d7010fd10042de4e8ca2ada5c3
Juan <juanalbertoh.fernandez13@gmail.com>
2026-06-12 23:38:15 +0200

    fix: añade actualizarDatosDetallePedido, añade el idPedido a actualizarDatosPedido y agregarUnNuevoProductoAlPedido

--- src/main/java/tfg/funkomania/funkomania_api/services/PedidoAdminService.java
+++ src/main/java/tfg/funkomania/funkomania_api/services/PedidoAdminService.java
@@ -17,13 +17,14 @@ import java.util.List;
 public interface PedidoAdminService {
     /**
      * Obtiene todos los pedidos de administrador que coinciden con los criterios de búsqueda proporcionados.
-     * @param idPedido El ID del pedido que se desea buscar (opcional).
+     *
+     * @param idPedido     El ID del pedido que se desea buscar (opcional).
      * @param codigoPedido El código del pedido que se desea buscar (opcional).
-     * @param usuario El nombre o email del usuario que realizó el pedido (opcional).
-     * @param fechaPedido La fecha del pedido que se desea buscar (opcional).
+     * @param usuario      El nombre o email del usuario que realizó el pedido (opcional).
+     * @param fechaPedido  La fecha del pedido que se desea buscar (opcional).
      * @param estadoPedido El estado del pedido que se desea buscar (opcional).
-     * @param estadoPago El estado del pago que se desea buscar (opcional).
-     * @param metodoPago El metodo de pago del pedido que se desea buscar (opcional).
+     * @param estadoPago   El estado del pago que se desea buscar (opcional).
+     * @param metodoPago   El metodo de pago del pedido que se desea buscar (opcional).
      * @return Una lista de DTOs que representan los pedidos de administrador que coinciden con los criterios de búsqueda proporcionados.
      */
     List<VistaPedidosAdminDTOId> getAllPedidosAdmin(
@@ -46,23 +47,34 @@ public interface PedidoAdminService {
 
     /**
      * Agrega una nueva línea de pedido a un pedido existente.
+     * @param idPedido                El ID del pedido al que se desea agregar la línea de pedido.
      * @param datosAgregarLineaPedido DTO que contiene la información necesaria para agregar una nueva línea de pedido.
      * @return Un DTO que contiene la información de la línea de pedido recién agregada, incluyendo su ID y detalles relevantes.
      */
-    PedidoCompletoDTOId agregarUnNuevoProductoAlPedido(AdminAgregarLineaPedidoRequestDTO datosAgregarLineaPedido);
+    PedidoCompletoDTOId agregarUnNuevoProductoAlPedido(Long idPedido, AdminAgregarLineaPedidoRequestDTO datosAgregarLineaPedido);
 
     /**
      * Actualiza los datos de un pedido específico.
-     * @param idPedido El ID del pedido que se desea actualizar.
+     * @param idPedido              El ID del pedido que se desea actualizar.
      * @param datosActualizarPedido DTO que contiene la información necesaria para actualizar el pedido.
-     * @return  Un DTO que contiene los detalles completos del pedido actualizado, incluyendo los totales y las líneas de pedido.
+     * @return Un DTO que contiene los detalles completos del pedido actualizado, incluyendo los totales y las líneas de pedido.
      */
     PedidoCompletoDTOId actualizarDatosPedido(Long idPedido, AdminUpdatePedidoRequestDTO datosActualizarPedido);
 
     /**
-     * Elimina una línea de pedido específica de un pedido existente.
-     * @param idPedido El ID del pedido del cual se desea eliminar la línea de pedido.
-     * @param idUsuario El ID del usuario que realizó el pedido, utilizado para verificar que el usuario tenga permiso para eliminar la línea de pedido.
+     * Actualiza una línea de pedido específica de un pedido existente.
+     *
+     * @param idPedido                     El ID del pedido del cual se desea actualizar la línea de pedido.
+     * @param idProducto                   El ID del producto que se desea actualizar en el pedido.
+     * @param datosActualizarDetallePedido DTO que contiene la información necesaria para actualizar el detalle del pedido.
+     * @return Un DTO que contiene los detalles completos del pedido actualizado, incluyendo los totales y las líneas de pedido.
      */
-    void eliminarProductoEnPedido(Long idPedido, Long idUsuario);
+    PedidoCompletoDTOId actualizarDatosDetallePedido(Long idPedido, Long idProducto, AdminUpdateProductoPedidoRequestDTO datosActualizarDetallePedido);
+
+    /**
+     * Elimina una línea de pedido específica de un pedido existente.
+     * @param idPedido   El ID del pedido del cual se desea eliminar la línea de pedido
+     * @param idProducto El ID del producto que se desea eliminar del pedido.
+     */
+    void eliminarDetallePedido(Long idPedido, Long idProducto);
 }
