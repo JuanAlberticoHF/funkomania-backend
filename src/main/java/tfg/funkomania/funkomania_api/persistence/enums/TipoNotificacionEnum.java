b63a51231c771d339078321086159a387449cfc8
Juan <juanalbertoh.fernandez13@gmail.com>
2026-06-12 23:13:51 +0200

    refactor: añade el comentario de no implementación a LISTADESEOS_STOCK

--- src/main/java/tfg/funkomania/funkomania_api/persistence/enums/TipoNotificacionEnum.java
+++ src/main/java/tfg/funkomania/funkomania_api/persistence/enums/TipoNotificacionEnum.java
@@ -11,7 +11,7 @@ package tfg.funkomania.funkomania_api.persistence.enums;
  * <li>{@code BIENVENIDA}: Notificación de bienvenida enviada a un nuevo usuario después de completar su registro.</li>
  *
  * @author JuanAlbeticoHF
- * @version 1.0.1
+ * @version 1.0.2
  * @since 0.5.0
  */
 public enum TipoNotificacionEnum {
@@ -20,6 +20,6 @@ public enum TipoNotificacionEnum {
     ESTADO_PEDIDO,
     CARRITO_ABANDONADO, // No implementado, reservado a futuro.
     PAGO_ERROR, // No implementado, reservado a futuro.
-    LISTADESEOS_STOCK,
+    LISTADESEOS_STOCK, // No implementado, reservado a futuro.
     BIENVENIDA
 }
