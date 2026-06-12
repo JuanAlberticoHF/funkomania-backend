41a71b2e358d9d6e36f1c34bf8e5687d6583ee48
Juan <juanalbertoh.fernandez13@gmail.com>
2026-06-12 18:25:48 +0200

    feat: añade un metodo para obtener el ultimo pedido del usuario.

--- src/main/java/tfg/funkomania/funkomania_api/persistence/repositories/IPedidoRepository.java
+++ src/main/java/tfg/funkomania/funkomania_api/persistence/repositories/IPedidoRepository.java
@@ -10,7 +10,7 @@ import tfg.funkomania.funkomania_api.persistence.entities.Pedido;
  * Interfaz de repositorio para la entidad Pedido.
  *
  * @author JuanAlbeticoHF
- * @version 1.0.0
+ * @version 1.1.0
  * @since 0.7.0
  */
 public interface IPedidoRepository extends JpaRepository<Pedido, Long> {
@@ -20,6 +20,9 @@ public interface IPedidoRepository extends JpaRepository<Pedido, Long> {
                                   @Param("p_idDireccion") Long idDireccion,
                                   @Param("p_idMetodoPago") Long idMetodoPago,
                                   @Param("p_comentarios") String comentarios);
+
+    @Query(value = "SELECT p FROM Pedido p WHERE p.usuario.idUsuario = :p_idUsuario order by p.fechaPedido desc limit 1")
+    Pedido obtenerElUltimoPedidoDelUsuario(@Param("p_idUsuario") Long idUsuario);
 
     @Modifying
     @Query(value = "CALL sp_cancelar_pedido(:p_idPedido)", nativeQuery = true)
