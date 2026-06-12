87633637bc9da0838a379478fe6d6c120f58f25b
Juan <juanalbertoh.fernandez13@gmail.com>
2026-06-12 20:21:09 +0200

    chore: cambia la versión del proyecto a la  v0.7.0-SNAPSHOT-3

--- src/main/java/tfg/funkomania/funkomania_api/config/OpenAPIConfig.java
+++ src/main/java/tfg/funkomania/funkomania_api/config/OpenAPIConfig.java
@@ -19,7 +19,7 @@ import org.springframework.context.annotation.Configuration;
 @OpenAPIDefinition(
         info = @Info(
                 title = "Funkomania API",
-                version = "0.7.0-SNAPSHOT-2",
+                version = "0.7.0-SNAPSHOT-3",
                 description = "Documentación de la API de Funkomania"
         )
 )
