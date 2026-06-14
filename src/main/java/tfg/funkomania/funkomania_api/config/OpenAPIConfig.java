3ebe837ebc7effb55660d4c15b480195767c9c47
Juan <juanalbertoh.fernandez13@gmail.com>
2026-06-14 10:48:08 +0200

    chore: actualiza la versión a la v0.8.2

--- src/main/java/tfg/funkomania/funkomania_api/config/OpenAPIConfig.java
+++ src/main/java/tfg/funkomania/funkomania_api/config/OpenAPIConfig.java
@@ -19,7 +19,7 @@ import org.springframework.context.annotation.Configuration;
 @OpenAPIDefinition(
         info = @Info(
                 title = "Funkomania API",
-                version = "0.8.1",
+                version = "0.8.2",
                 description = "Documentación de la API de Funkomania"
         )
 )
