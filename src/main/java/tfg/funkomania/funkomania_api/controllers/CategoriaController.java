ed2779d202d06c96ee31ec324346cb8c0cd6dddb
Juan <juanalbertoh.fernandez13@gmail.com>
2026-06-15 12:09:18 +0200

    refactor: elimina un import sin utilizar.

--- src/main/java/tfg/funkomania/funkomania_api/controllers/CategoriaController.java
+++ src/main/java/tfg/funkomania/funkomania_api/controllers/CategoriaController.java
@@ -9,7 +9,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
